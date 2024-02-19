6599eef47ab13436f14bd8dad639add1a3e33aaf
Agustin Padilla Saboredo <AP307060@fi365.ort.edu.uy>
2024-02-18 21:18:48 -0300

    un par de cosa

--- js/script.js
+++ js/script.js
@@ -32,7 +32,8 @@ function inicio() {
 
 function eventos() {
   ROUTER.addEventListener("ionRouteDidChange", navegar)
-
+  BTNREGISTRARU.addEventListener('click', getPaises)
+  BTNREGISTRARC.addEventListener('click', getAlimentos)
 }
 
 function navegar(event) {
@@ -81,38 +82,38 @@ function chequearSesion() {
   ocultarBotones()
   if (localStorage.getItem("apiKey") != null) {
     mostrarMenuUsuario()
-  }else{
+  } else {
     mostrarMenuAnon()
   }
 }
 
 function ocultarBotones() {
-  BTNHOME.style.display="none"
-  BTNLOGIN.style.display="none"
-  BTNREGISTRARU.style.display="none"
-  BTNREGISTRARC.style.display="none"
-  BTNLISTARC.style.display="none"
-  BTNINFORME.style.display="none"
-  BTNMAPA.style.display="none"
-  BTNCERRARSESION.style.display="none"
+  BTNHOME.style.display = "none"
+  BTNLOGIN.style.display = "none"
+  BTNREGISTRARU.style.display = "none"
+  BTNREGISTRARC.style.display = "none"
+  BTNLISTARC.style.display = "none"
+  BTNINFORME.style.display = "none"
+  BTNMAPA.style.display = "none"
+  BTNCERRARSESION.style.display = "none"
 }
 
-function mostrarMenuUsuario(){
-  BTNHOME.style.display="block"
-  BTNREGISTRARC.style.display="block"
-  BTNLISTARC.style.display="block"
-  BTNINFORME.style.display="block"
-  BTNMAPA.style.display="block"
-  BTNCERRARSESION.style.display="block"
+function mostrarMenuUsuario() {
+  BTNHOME.style.display = "block"
+  BTNREGISTRARC.style.display = "block"
+  BTNLISTARC.style.display = "block"
+  BTNINFORME.style.display = "block"
+  BTNMAPA.style.display = "block"
+  BTNCERRARSESION.style.display = "block"
 }
 
 function mostrarMenuAnon() {
-  BTNHOME.style.display="block"
-  BTNLOGIN.style.display="block"
-  BTNREGISTRARU.style.display="block"
+  BTNHOME.style.display = "block"
+  BTNLOGIN.style.display = "block"
+  BTNREGISTRARU.style.display = "block"
 }
 
-function previaLogin(){
+function previaLogin() {
   let usuario = document.querySelector('#loginUsuario')
   let password = document.querySelector('#loginPassword')
 
@@ -122,25 +123,123 @@ function previaLogin(){
   login(nuevoUsuario)
 }
 
-function login(nuevoUsuario){
-  fetch(`${URLBASE}/login.php`,{
-    method:'POST',
-    headers:{
-      'Content-Type':'application/json'
+function login(nuevoUsuario) {
+  fetch(`${URLBASE}/login.php`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(nuevoUsuario)
   })
-  .then(response => {
-    return response.json()
-  })
-  .then(data => {
-    localStorage.setItem('apiKey', data.apiKey)
-    chequearSesion()
-  })
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      iniciarSesion(data)
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
 }
 
-function cerrarSesion(){
+function cerrarSesion() {
   localStorage.removeItem('apiKey')
   cerrarMenu()
   chequearSesion()
+}
+
+function getPaises() {
+  fetch(`${URLBASE}/paises.php`)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      cargarListaPaises(data.paises)
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+}
+
+function cargarListaPaises(paises) {
+  result = ''
+  for (let pais of paises) {
+    result += `<ion-select-option ${pais.id}>${pais.name}</ion-select-option>`
+  }
+  document.querySelector('#registroPais').innerHTML = result
+}
+
+function previaRegistroU() {
+  let usuario = document.querySelector('#registroUsuario').value
+  let pass = document.querySelector('#registroPassword').value
+  let calorias = document.querySelector('#caloriasDiarias').value
+  let pais = document.querySelector('#registroPais').value
+
+  let nuevoUsuario = {}
+  nuevoUsuario.usuario = usuario
+  nuevoUsuario.password = pass
+  nuevoUsuario.caloriasDiarias = calorias
+  nuevoUsuario.idPais = pais
+
+  registroUsuario(nuevoUsuario)
+}
+
+function registroUsuario(nuevoUsuario) {
+  fetch(`${URLBASE}/usuarios.php`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(nuevoUsuario)
+  })
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      if (data.mensaje == null) {
+        iniciarSesion(data)
+      } else {
+        alert(data.mensaje)
+      }
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+}
+
+function iniciarSesion(data) {
+  localStorage.setItem('apiKey', data.apiKey)
+  localStorage.setItem('idUsuario', data.id)
+  chequearSesion()
+  ocultarPantallas()
+  HOME.style.display = "block"
+}
+
+function getAlimentos() {
+  fetch(`${URLBASE}/alimentos.php`,{
+    headers: {
+      'Content-Type': 'application/json',
+      'apikey': localStorage.getItem('apiKey'),
+      'iduser': localStorage.getItem('idUsuario')
+    }
+  }
+  )
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      const listaAlimentos = data.alimentos
+      cargarListaAlimentos(data.alimentos)
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+}
+
+function cargarListaAlimentos(alimentos) {
+  result = ''
+  for (let alimento of alimentos) {
+    result += `<ion-select-option ${alimento.id}>${alimento.nombre}</ion-select-option>`
+  }
+  document.querySelector('#registroAlimento').innerHTML = result
 }
